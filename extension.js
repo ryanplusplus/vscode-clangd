f1597390249513d193ed0651afa24dc5929f89d3
Ryan Hartlage <ryanplusplus@gmail.com>
2024-11-13 08:46:06 -0500

    Neutered

---
+++ extension.js
@@ -0,0 +1,13 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
